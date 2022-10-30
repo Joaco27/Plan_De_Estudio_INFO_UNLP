71f892cc717e7cf8d33605c751007b87a88dcefc
Joaco27 <joaco200127@gmail.com>
2022-10-30 20:33:16 -0300

    Actualizando Contenido

--- analisis.js
+++ analisis.js
@@ -1,3 +1,8 @@
 function analizar(){
-    console.log("Analizando Datos")
+    alert("Analizando Estado de la Carrera")
+}
+
+function detallar(){
+    const m = prompt("Ingrese Materia")
+    console.log(m)
 }
