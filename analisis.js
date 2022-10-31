992b28216f50d0db55a44c1fe5e46fbe53146cfa
Joaco27 <joaco200127@gmail.com>
2022-10-31 10:04:27 -0300

    Agregando Funcionalidades

--- analisis.js
+++ analisis.js
@@ -2,7 +2,17 @@ function analizar(){
     alert("Analizando Estado de la Carrera")
 }
 
-function detallar(){
-    const m = prompt("Ingrese Materia")
-    console.log(m)
+function trarP(){
+    let p = document.getElementsByClassName("materia")
+    return p[0]
 }
+
+function detallar(){
+    let p = trarP()
+    let m = prompt("Ingrese Materia")
+    let materia = document.getElementsByClassName(m)
+    materia = materia[0].getElementsByTagName("td")
+    p.innerHTML = `Nombre de Materia: ${materia[0].innerHTML} <br> Codigo: ${materia[1].innerHTML} <br> 
+        Estado: ${materia[3].innerHTML}`
+}
+
